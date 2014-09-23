b80d6744b4f47e4f7058442ad3b586556b0954c4
731942771 <731942771@qq.com>
2014-09-23 15:19:54 +0000

    02,20140923-15:22，MobileGuard手机安全卫士欢迎界面

--- src/com/cuiweiyou/mobileguard/WelcomeActivity.java
+++ src/com/cuiweiyou/mobileguard/WelcomeActivity.java
@@ -1,34 +1,60 @@
 package com.cuiweiyou.mobileguard;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
-public class WelcomeActivity extends ActionBarActivity {
+/**
+ * 第一个界面的作用：
+ * 1.展示产品Logo
+ * 2.应用程序初始化（元数据-数据库、配置信息、尺寸、登录信息）
+ * 3.检查新版本
+ * 4.检查当前程序的注册信息、版权
+ * 5.检查网络
+ * 6.。。。。。。
+ */
+/**
+ * 创建项目时，Theme为None。本Activity继承自ActionBarActivity，测试时报错： Unable to start
+ * activity ComponentInfo : java.lang.IllegalStateException: You need to use a
+ * Theme.AppCompat theme (or descendant) with this activity.
+ */
+public class WelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+
+		// 获取控件
+		TextView tv_version = (TextView) findViewById(R.id.tv_version);
+		// 更新文字
+		tv_version.setText("版本 " + getVersionName());
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.welcome, menu);
-		return true;
-	}
+	/***
+	 * 动态获取应用版本
+	 */
+	private String getVersionName() {
+		// 1.应用包管理器，管理应用程序包,通过它就可以获取应用程序信息
+		PackageManager packageManager = getPackageManager();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		try {
+			// 2.应用包信息，获取AndroidManifest.xml中的配置信息
+			PackageInfo packageInfo = packageManager.getPackageInfo(
+					getPackageName(), 0);
+
+			// 3.版本名
+			String versionName = packageInfo.versionName;
+
+			// 4.return
+			return versionName;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return super.onOptionsItemSelected(item);
+
+		return "读取失败";
 	}
+
 }
