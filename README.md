38eaa8cc48e61bfbbe35362c905afd9175527a59
731942771 <731942771@qq.com>
2014-09-24 16:09:27 +0000

    04,20140924-16:10，MobileGuard，更新功能实现（数字签名）

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# 这个README.md须在Eclipse里创建
-
-手机安全卫士
-
-创建：20140923
-Author：Vigiles
-
-最低版本：2.3.3
-目标版本：4.4
-编译版本：4.4
+# 这个README.md须在Eclipse里创建<br/>
+<br/>
+手机安全卫士<br/>
+<br/>
+创建：20140923<br/>
+Author：Vigiles<br/>
+<br/>
+最低版本：2.3.3<br/>
+目标版本：4.4<br/>
+编译版本：4.4<br/>
+如果在服务器上修改了项目，必须首先在客户端下载，否则就是
+rejected non fast forward错误
--- src/com/cuiweiyou/mobileguard/WelcomeActivity.java
+++ src/com/cuiweiyou/mobileguard/WelcomeActivity.java
@@ -1,21 +1,38 @@
 package com.cuiweiyou.mobileguard;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.HttpHandler;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
 
 /**
 ~~~第2次提交~~~
@@ -37,18 +54,30 @@ import android.widget.Toast;
  * Theme.AppCompat theme (or descendant) with this activity.
  */
 public class WelcomeActivity extends Activity {
+	/** 新版本apk文件路径 **/
+	private String apkurl;
 
-	// 消息代码,进入主界面
+	/** 更新信息显示控件 **/
+	private TextView tv_download_info;
+
+	/** 消息代码,进入主界面 **/
 	protected static final int ENTER_HOME = 0;
-	// 消息代码,更新提示
+	/** 消息代码,弹出更新提示Dialog **/
 	protected static final int UPDATE_DIALOG = 1;
-	// 消息代码,异常状况
-	protected static final int Exception_DIALOG = 2;
+	/** 消息代码,网络路径无效错误 **/
+	protected static final int URL_EXCEPTION_DIALOG = 2;
+	/** 消息代码,数据传输协议不匹配而导致无法与远程方进行通信 **/
+	protected static final int PROTOCOL_EXCEPTION_DIALOG = 3;
+	/** 消息代码,网络读写错误 **/
+	protected static final int IO_EXCEPTION_DIALOG = 4;
+	/** 消息代码,JSON数据错误 **/
+	protected static final int JSON_EXCEPTION_DIALOG = 5;
 	
 	/**
 	~~~第3次提交~~~
 	
-	 * 异步的消息处理器，根据消息作出反应 
+	 * 异步的消息处理器，根据消息作出反应：
+	 * 直接进入主界面 / 提示更新 / 提示错误信息 
 	 **/
 	Handler handler = new Handler(){
 		// 处理消息
@@ -60,13 +89,148 @@ public class WelcomeActivity extends Activity {
 					enterHomeActivity();
 				break;
 				case UPDATE_DIALOG:	// 更新提示
-					Toast.makeText(getApplicationContext(), "有新版本啦", 0).show();
-					enterHomeActivity();
+					
+					/**
+					 * ~~~第4次提交~~~
+					 * 
+					 * 检查更新功能进一步实现
+					 * 使用XUtils（原afinal）https://github.com/wyouflf/xUtils
+					 **/
+					//Toast.makeText(getApplicationContext(), "有新版本啦", 0).show();
+					
+					/** 1.对话框的创建器 */
+					AlertDialog.Builder dialog = new Builder(WelcomeActivity.this);
+					
+					/** 2.标题 */
+					dialog.setTitle("升级提示");
+					
+					/** 3.信息内容 */
+					dialog.setMessage("现在有新版本，立刻下载更新");
+					
+					/** 4.确定按钮（文本，事件） */
+					dialog.setPositiveButton("确定", new OnClickListener() {
+						/** dialog-本提示框，which-点击的按钮 */
+						public void onClick(DialogInterface dialog, int which) {
+							/** 获取外部存储-TF卡的状态，如果可用 */
+							if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+								// 创建工具
+								HttpUtils http = new HttpUtils();
+								// 最大开启线程数量，一般3、4个
+								http.configRequestThreadPoolSize(1);
+								// 执行下载
+								http.download(
+									// 源路径
+									apkurl,
+									
+									// 目标路径
+									//Environment.getExternalStorageDirectory().getAbsolutePath() + "MobilGuard.apk",	// 错误：IOException: open failed: EROFS (Read-only file system)
+									Environment.getExternalStorageDirectory().getAbsolutePath() + "/MobilGuard.apk",	// 注意加斜杠
+									// "/mnt/sdcard/MobilGuard.apk",													// 4.4.2对应 /storage/sdcard/MobilGuard.apk，4.1.2对应/mnt/sdcard/Mo---
+									
+									// 回调，每秒一次
+									new RequestCallBack<File>(){
+
+										// 下载失败
+										public void onFailure(HttpException exception, String msg) {
+											exception.printStackTrace();
+											Toast.makeText(getApplicationContext(), "下载失败：" + msg, 0).show();
+										}
+										
+										// 下载进行中---
+										public void onLoading(long total, long current, boolean isUploading) {
+											System.err.println("总量：" + total);
+											System.err.println("排量：" + current);
+											
+											// 下载比例
+											long progress = current * 100 / total;
+											if ( ! isUploading){
+												tv_download_info.setText("当前下载：" + progress + "%");
+											}
+										}
+
+										// 下载成功
+										public void onSuccess(ResponseInfo<File> info) {
+											tv_download_info.setText("下载结束了");
+											// 要使用info.result.getPath()，须 extends Activity
+											System.err.println("文件：" + info.result.getPath() + ".");
+											
+/*											// 1.意图
+											Intent intent = new Intent();
+											// 2.打开新界面。对应apk文件，打开安装器
+											intent.setAction("android.intent.action.VIEW");
+											// 3.意图类型
+											intent.addCategory("android.intent.category.DEFAULT");
+											// 4.目标文件
+											// info.result.getPath() = (4.4.2)/storage/sdcard/MobilGuard.apk，(4.1.2)/mnt/sdcard/m----
+											File apk = new File(info.result.getPath().trim());
+											// 5.意图操作的文件和文件类型，也就是打开这个文件，开始安装
+											intent.setDataAndType(Uri.fromFile(apk), "application/vnd.android.apckage-archive");
+											// 6.实施意图
+											
+-------------------------------------------------------------这里报错-------------------------------------------------------
+09-24 00:04:22.549: E/AndroidRuntime(1818): 
+android.content.ActivityNotFoundException: 
+ No Activity found to handle Intent { 
+	 act=android.intent.action.VIEW 
+	 cat=[android.intent.category.DEFAULT] 
+	 dat=file:///storage/sdcard/MobilGuard.apk 
+	 typ=application/vnd.android.apckage-archive 
+ }
+											startActivity(intent);
+**///--------------------------------------------以下是替换方法--------------------------------------------------------------
+											// 1.创建意图
+											Intent intent = new Intent(Intent.ACTION_VIEW);	// 同上
+											// 2.意图实施对象（文件，文件类型）
+											intent.setDataAndType(
+													Uri.parse("file://"+ Environment.getExternalStorageDirectory().getAbsolutePath() + "/MobilGuard.apk"),
+													"application/vnd.android.package-archive");
+											// 3.实施。弹出安装应用程序界面
+											startActivity(intent);
+											
+											/**
+											 * 安卓应用的数字签名
+											 * http://www.cuiweiyou.com/583.html
+											 */
+										}
+									});
+							} else {
+								Toast.makeText(getApplicationContext(), "内存卡读取失败，请重新安装后尝试", 0).show();
+								return;
+							}
+						}
+					});
+					
+					/** 5.取消按钮（文本，事件） **/
+					dialog.setNegativeButton("取消", new OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							/** 什么都不写默认实现就是关闭掉对话框 **/
+						}
+					});
+					
+					/** 6.禁用系统的返回功能 **/
+					dialog.setCancelable(false);// 在这个Handler里达不到禁用返回键的效果
+					
+					/** 7.显示对话框 **/
+					dialog.show();	// 其中调用create方法
+					
+					// enterHomeActivity();
 				break;
-				case Exception_DIALOG:	// 异常情况提示
-					Toast.makeText(getApplicationContext(), "有点意外，下次更新吧", 0).show();
-					enterHomeActivity();
+				case URL_EXCEPTION_DIALOG:	// 异常情况提示
+					Toast.makeText(getApplicationContext(), "网络路径无效错误，下次更新吧", 5).show();
+					// enterHomeActivity();
 				break;
+				case PROTOCOL_EXCEPTION_DIALOG:	// 异常情况提示
+					Toast.makeText(getApplicationContext(), "数据传输协议不匹配而导致无法与远程方进行通信，下次更新吧", 5).show();
+					// enterHomeActivity();
+					break;
+				case IO_EXCEPTION_DIALOG:	// 异常情况提示
+					Toast.makeText(getApplicationContext(), "网络读写错误，下次更新吧", 5).show();
+					// enterHomeActivity();
+					break;
+				case JSON_EXCEPTION_DIALOG:	// 异常情况提示
+					Toast.makeText(getApplicationContext(), "JSON数据错误，下次更新吧", 5).show();
+					// enterHomeActivity();
+					break;
 			}
 			
 		}
@@ -79,8 +243,10 @@ public class WelcomeActivity extends Activity {
 
 		// 获取控件
 		TextView tv_version = (TextView) findViewById(R.id.tv_version);
-		// 更新文字
+		// 当前版本
 		tv_version.setText("版本 " + getVersionName());
+		// 更新信息
+		tv_download_info = (TextView) findViewById(R.id.tv_download_info);
 
 		/** 
 		~~~第3次提交~~~
@@ -114,7 +280,7 @@ public class WelcomeActivity extends Activity {
 				
 				// 启动时间
 				long startTime = System.currentTimeMillis();
-				
+			
 				try {
 					// R.string.updateurl。在/res/values/里新建config.xml，保存更新请求网址
 					URL updateurl = new URL(getString(R.string.updateurl));
@@ -146,40 +312,55 @@ public class WelcomeActivity extends Activity {
 						// 关闭输出流
 						baos.close();
 						
+						/****
+						 * 05-26 13:34:14.447: W/System.err(2888): }
+						 * 05-26 13:34:14.447: W/System.err(2888): org.json.JSONException: 
+						 * 	Value ﻿ of type java.lang.String cannot be converted to JSONObject
+						 * 解决：用notepad++将json文件以UTF-8无BOM格式编码
+						 */
 						/** JSON数据处理 **/
 						JSONObject jsonObject = new JSONObject(updateinfo);
 						// 根据key获取数据
-						String version = (String) jsonObject.get("version");
-						String description = (String) jsonObject.get("description");
-						String apkurl = (String) jsonObject.get("apkurl");
+						String version = (String) jsonObject.get("version");			// 新的版本
+						String description = (String) jsonObject.get("description");	// 新版描述
+						apkurl = (String) jsonObject.get("apkurl");						// 新版apk下载路径
 						
 						// 判断版本
 						if(getVersionName().equals(version)){
 							// 消息
-							message.what=ENTER_HOME;	// 进入应用界面
+							message.what=ENTER_HOME;	// 版本相同，无更新，进入应用界面
 						}else{
-							message.what=UPDATE_DIALOG;	// 更新对话框
+							message.what=UPDATE_DIALOG;	// 版本不同，有更新，弹出更新对话框
 						}
 					}
-				} catch (Exception e) {
+				} catch (MalformedURLException e) {
+					message.what=URL_EXCEPTION_DIALOG;	// URL无效错误
 					e.printStackTrace();
-					message.what=Exception_DIALOG;	// 异常情况提示
+				} catch (ProtocolException e) {
+					message.what=PROTOCOL_EXCEPTION_DIALOG;	// 数据传输协议不匹配而导致无法与远程方进行通信
+					e.printStackTrace();
+				} catch (IOException e) {
+					message.what=IO_EXCEPTION_DIALOG;	// 网络读写错误
+					e.printStackTrace();
+				} catch (JSONException e) {
+					message.what=JSON_EXCEPTION_DIALOG;	// JSON数据错误
+					e.printStackTrace();
 				} finally{
 					// 网络访问结束时间
 					long endTime = System.currentTimeMillis();
 					// 耗时-也就是欢迎界面的显示时长
 					long distanceTime = endTime - startTime;
-					// 欢迎界面至少显示2秒
-					if( distanceTime < 2000){
+					// 欢迎界面至少显示3秒
+					if( distanceTime < 3000){
 						try {
-							// 不到2s就等一会儿
-							Thread.sleep(2000 - distanceTime);
+							// 不到3s就等一会儿
+							Thread.sleep(3000 - distanceTime);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
 					
-					/** 向处理器发送消息 **/
+					/** 向处理器发送消息代码 **/
 					handler.sendMessage(message);
 				}
 			}
@@ -203,7 +384,7 @@ public class WelcomeActivity extends Activity {
 	/***
 	 * ~~~第2次提交~~~
 	 * 
-	 * 动态获取应用版本
+	 * 动态获取当前应用版本
 	 */
 	private String getVersionName() {
 		// 1.应用包管理器，管理应用程序包,通过它就可以获取应用程序信息
